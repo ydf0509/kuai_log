71a8dab4536b5967d97b45775df9ac4c9a152c15
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-09-15 20:49:34 +0800

    commit

--- tests/test_timestr.py
+++ tests/test_timestr.py
@@ -5,9 +5,14 @@ import time
 # print(time.gmtime())
 
 from kuai_log._datetime import datetime,aware_now
-
+import pysnooper_click_able
 # datetime.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
-for i in range(1000000):
-    if i %1000 == 0:
-        print(aware_now())
+@pysnooper_click_able.snoop(depth=10000)
+def f():
+    for i in range(1):
+        if i %1000 == 0:
+            print(aware_now())
+
+
+f()
