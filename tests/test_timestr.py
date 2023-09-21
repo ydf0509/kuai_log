e49b44a16b3685941e45e20f977afe347d621fc6
ydf0509 <909686719@qq.com>
2023-09-21 22:43:33 +0800

    commit

--- tests/test_timestr.py
+++ tests/test_timestr.py
@@ -3,16 +3,24 @@
 import time
 
 # print(time.gmtime())
-
+from datetime import datetime as dt
 from kuai_log._datetime import datetime,aware_now
 import pysnooper_click_able
 # datetime.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
-@pysnooper_click_able.snoop(depth=10000)
+t1 = time.time()
+# @pysnooper_click_able.snoop(depth=10000)
 def f():
-    for i in range(1):
-        if i %1000 == 0:
-            print(aware_now())
+    for i in range(1000000):
+        if i %10000 == 0:
+            aware_now()
+            # print(aware_now())
+            # print(dt.now().strftime('%Y-%m-%dT%H:%M:%S %z'))
+            # now = dt.now()
+            # year, month, day, hour, minute, second, weekday, yearday, xx = now.timetuple()
+            # microsecond = now.microsecond
+            # print(year, month, day, hour, minute, second, weekday, yearday,xx,microsecond)
 
 
 f()
+print(time.time() -t1)
